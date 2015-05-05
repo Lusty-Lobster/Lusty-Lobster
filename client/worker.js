f4e59d0eab31adecad24c5d7d413d0131c675a4a
Katrina Uychaco <kuychaco@gmail.com>
2015-05-05 15:44:41 -0700

    Build webworker.
    - Accept algorithm and data and run algorithm using data.

--- client/worker.js
+++ client/worker.js
@@ -1,6 +1,16 @@
-var alg = function (n){s=0;function f(l,o,r,c,t,v){v=~(l|o|r)&c;while(v>0){v^=t=-v&v;f((l|t)<<1,o|t,(r|t)>>1,c)}o==c&&s++}f(0,0,0,(1<<n)-1);return s};
-var data = 15;
+onmessage = function (e) {
+  console.log('Algorithm and data recieved from Rabbit Hole');
+  var alg = e.data[0];
+  var data = e.data[1];
 
-var result = alg(data);
+  var result = eval(alg)(data);
+  postMessage(result);
+};
 
-postMessage(result);
+
+// var alg = function (n){s=0;function f(l,o,r,c,t,v){v=~(l|o|r)&c;while(v>0){v^=t=-v&v;f((l|t)<<1,o|t,(r|t)>>1,c)}o==c&&s++}f(0,0,0,(1<<n)-1);return s};
+// var data = 15;
+
+// var result = alg(data);
+
+// postMessage(result);
