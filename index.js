6b63cdf3e3cac901aa08721d78ee856c4ecb0e54
John Winstead <awatemonosan@gmail.com>
2015-05-07 09:06:56 -0700

    More tweaks to get heroku working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('./server/server.js');
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log('Node app is running at localhost:' + app.get('port'));
 });
