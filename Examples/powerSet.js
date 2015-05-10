08dda8a989a73cf797ab05e0729e87366d117be7
Joseph Lin <josephlin55555@gmail.com>
2015-05-10 14:14:27 -0700

    Implement the word version of powerSet

--- Examples/powerSet.js
+++ Examples/powerSet.js
@@ -1,3 +1,4 @@
+//powerset for letters
 var powerSet = function(string) {
   if(string === undefined) {
     string = '';
@@ -26,5 +27,33 @@ var powerSet = function(string) {
   return solutions.sort();
 };
 
+//powerset for complete words
+var powerSetWord = function(string) {
+  if(string === undefined) {
+    string = '';
+  }
+
+  string = string.split(' ');
+
+  var letters = {};
+  for(var i = 0; i < string.length; i++) {
+    letters[string[i]] = true;
+  }
+
+  var solutions = [];
+  var recurse = function(index, subset) {
+    if(index >= string.length) {
+      solutions.push(subset);
+      return;
+    }
+
+    recurse(index + 1, subset);
+    recurse(index + 1, subset + ' ' + string[index] + ' '); //can take ' ' away in the middle if concatenation is required
+  };
+
+  recurse(0, '');
+
+  return solutions.sort();
+};
 
 
